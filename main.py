edc7e76eb9865c07bb075c2cd8519ebe889447bc
Number1Tao <taojiang_ict@163.com>
2017-02-11 22:10:34 +0800

    V2 unpack to .exe file for windows slove problem1 :https://groups.google.com/forum/#!topic/pulp-or-discuss/3bS3KIc9-QM slove problem2: file depend

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ rlt_path=""
 import config
 import optimize
 
+print u'结果：需要调的集装箱'
 print optimize.opt()
+raw_input(u'按下enter键退出')
--- optimize.py
+++ optimize.py
@@ -3,6 +3,7 @@ rlt_path=""
 import config
 import file
 from pulp import *
+import os
 
 def opt():
     A,B,candinates=file.get()
@@ -16,8 +17,13 @@ def opt():
     #object
     obj=lpSum(X)
     prob+=obj
+
     #slove problem
-    solution=prob.solve()
+    cwd = os.getcwd()
+    solverdir = 'Cbc-2.7.5-win64-intel11.1\\bin\\cbc.exe'  # extracted and renamed CBC solver binary
+    solverdir = os.path.join(cwd, solverdir)
+    solver = pulp.COIN_CMD(path=solverdir)
+    solution=prob.solve(solver)
     #output result
     schedule=[]
     if LpStatus[solution]=='Optimal': #has optimal solution
