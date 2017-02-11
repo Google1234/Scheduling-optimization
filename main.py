e128885048d360d2b4544ee6bdd8f5f8e48a1fbd
Number1Tao <taojiang_ict@163.com>
2017-02-11 23:54:16 +0800

    V3.2

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # -*- coding: UTF-8 -*-
+import sys
 try:
 	rlt_path=""
 	import config
 	import optimize
-
+	rst=optimize.opt()
 	print u'结果：需要调的集装箱'
-	print optimize.opt()
-except:
-	print u'无解！'
+	print rst
+except :
+    print "Unexpected error:", sys.exc_info()[0]
 raw_input('press enter to exit!')
